439ddbabba07f8cd93f9054b8740534253193a96
yukiagatsuma <yukiagatsuma@gmail.com>
2018-04-24 01:30:27 +0900

    update application.rb
    jsとhelperファイルを作成できないように変更

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
+      g.javascripts false
+      g.helper false
       g.test_framework false
     end
     # Settings in config/environments/* take precedence over those specified here.
