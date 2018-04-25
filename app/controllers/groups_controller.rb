9ecc638bafdd06d6b50df7b7208d9db8fc4c3d50
yukiagatsuma <yukiagatsuma@gmail.com>
2018-04-25 17:47:18 +0900

    add controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,20 @@
+class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+  def group_params
+    params.permit()
+  end
+
+end
