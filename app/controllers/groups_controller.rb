852b6f780a2732c9aa507a302395c2fbcefb7787
yukiagatsuma <yukiagatsuma@gmail.com>
2018-04-26 21:41:04 +0900

    update groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,18 @@
 class GroupsController < ApplicationController
 
   def new
-  end
-
-  def edit
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def update
@@ -14,7 +20,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.permit()
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 
 end
