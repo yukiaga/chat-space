02882a4a65231e3a368a8ec8605f827a41cec743
yukiagatsuma <yukiagatsuma@gmail.com>
2018-05-05 21:16:36 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :messages
-  has_many :group_uers
+  has_many :group_users
   has_many :groups, through: :group_users
 end
