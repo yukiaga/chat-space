fca2e50925aaee86698e860db558f2c14e72e8ce
yukiagatsuma <yukiagatsuma@gmail.com>
2018-05-22 10:51:44 +0900

    in order to test user & message model, controller

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,6 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   has_many :messages, dependent: :destroy
-
   validates :name, presence: true
 
   def show_last_message
