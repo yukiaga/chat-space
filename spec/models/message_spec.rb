7126c71373bf57e0c27342c0356893afa69734d3
yukiagatsuma <yukiagatsuma@gmail.com>
2018-05-30 18:04:24 +0900

    update message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe '#create' do
-
     context 'can save' do
       it 'is valid with content' do
-        expect(build(:message, image: nil)).to be_vaild
+        expect(build(:message, image: nil)).to be_valid
       end
 
-      it "is valid with image" do
-        expect(build(:message, content: nil)).to be_vaild
+      it 'is valid with image' do
+        expect(build(:message, content: nil)).to be_valid
       end
 
-      it "is valid with content or image" do
-        expect(build(:message)).to be_vaild
+      it 'is valid with content and image' do
+        expect(build(:message)).to be_valid
       end
     end
 
@@ -30,7 +29,7 @@ RSpec.describe Message, type: :model do
         expect(message.errors[:group]).to include('を入力してください')
       end
 
-      it 'is invalid without user_id' do
+      it 'is invaid without user_id' do
         message = build(:message, user_id: nil)
         message.valid?
         expect(message.errors[:user]).to include('を入力してください')
